73d1bc75ce9138f6fa4c80d44589b3fa6ba584c7
Raghu Betina <raghu@firstdraft.com>
2016-09-28 03:17:39 +0000

    Whitelist columns in CoverPhoto for ActiveAdmin

--- app/admin/cover_photo.rb
+++ app/admin/cover_photo.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register CoverPhoto do
 
+ permit_params :restaurant_id, :cover_photo
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
