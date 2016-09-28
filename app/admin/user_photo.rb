6a2d3ee21ed0d4a8ff2c73682081dd5a2e164760
Raghu Betina <raghu@firstdraft.com>
2016-09-28 03:17:35 +0000

    Whitelist columns in UserPhoto for ActiveAdmin

--- app/admin/user_photo.rb
+++ app/admin/user_photo.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register UserPhoto do
 
+ permit_params :image, :restaurant_id
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
